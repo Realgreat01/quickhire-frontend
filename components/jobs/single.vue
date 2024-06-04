8a14870600fc447ab1fbe87870c270621a330a56
Realgreat <samsonrealgreat@gmail.com>
2024-06-04 15:29:19 +0100

    fixed minor error

--- components/jobs/single.vue
+++ components/jobs/single.vue
@@ -5,7 +5,7 @@
       <div class="grid grid-cols-2 items-start justify-between md:flex">
         <div class="flex w-full gap-x-2">
           <img
-            :src="job.posted_by?.logo"
+            :src="job?.posted_by?.logo"
             alt=""
             class="block max-h-12 max-w-12"
           />
@@ -13,23 +13,23 @@
           <div class="flex flex-col gap-2">
             <div class="flex gap-4">
               <div class="">
-                <h1 class="qh-text-3 font-bold">{{ job.job_title }}</h1>
+                <h1 class="qh-text-3 font-bold">{{ job?.job_title }}</h1>
                 <h1 class="qh-text-5 -mt-1 font-semibold text-brand">
-                  {{ job.posted_by?.company_name }}
+                  {{ job?.posted_by?.company_name }}
                 </h1>
               </div>
               <qh-button
                 class="!h-6 rounded-full !py-2 px-4 text-xs capitalize"
                 :class="{
-                  '!bg-success-500': job.job_status === 'open',
+                  '!bg-success-500': job?.job_status === 'open',
                   'disabled !bg-dark-500 !text-dark-100':
-                    job.job_status === 'paused',
-                  'disabled !bg-error': job.job_status === 'closed',
+                    job?.job_status === 'paused',
+                  'disabled !bg-error': job?.job_status === 'closed',
                 }"
                 :disabled="
-                  job.job_status === 'closed' || job.job_status === 'paused'
+                  job?.job_status === 'closed' || job?.job_status === 'paused'
                 "
-                :label="job?.job_status === 'open' ? 'Active' : job.job_status"
+                :label="job?.job_status === 'open' ? 'Active' : job?.job_status"
               />
               <qh-button
                 v-if="job?.is_new"
@@ -44,13 +44,13 @@
                 class="qh-text-5 flex h-4 items-center gap-x-2 rounded-full bg-dark-100 !px-3 !py-3 text-xs capitalize !text-brand-700"
               >
                 <RiHomeOfficeLine class="h-4 w-4" />
-                <p class="text-xs">{{ job.job_location_type }}</p>
+                <p class="text-xs">{{ job?.job_location_type }}</p>
               </qh-button>
               <qh-button
                 class="qh-text-5 flex h-4 items-center gap-x-2 rounded-full bg-dark-100 !px-3 !py-3 text-xs capitalize !text-brand-700"
               >
                 <RiBriefcaseLine class="h-4 w-4" />
-                <p class="text-xs">{{ job.job_type }}</p>
+                <p class="text-xs">{{ job?.job_type }}</p>
               </qh-button>
               <qh-button
                 class="qh-text-5 flex h-4 items-center gap-x-2 rounded-full bg-dark-100 !px-3 !py-3 text-xs capitalize !text-brand-700"
@@ -64,7 +64,7 @@
                 <RiHourglassLine class="h-4 w-4" />
                 <p class="">
                   {{
-                    `${job?.job_duration.value} ${job.job_duration.date}${job?.job_duration.value >= 1 ? 's' : ''}`
+                    `${job?.job_duration.value} ${job?.job_duration.date}${job?.job_duration.value >= 1 ? 's' : ''}`
                   }}
                 </p>
               </qh-button>
@@ -74,13 +74,13 @@
                 class="qh-text-4 flex h-4 items-center gap-x-2 rounded-full !bg-success-100 px-4 text-xs font-semibold !text-success-500"
               >
                 <RiCashLine class="h-4 w-4" />
-                {{ qhNumbers.formatCurrency(job.salary.value) }}
+                {{ qhNumbers.formatCurrency(job?.salary.value) }}
               </qh-button>
               <qh-button
                 class="qh-text-4 flex h-4 items-center gap-x-2 rounded-full !bg-error-100 px-4 text-xs font-semibold !text-error-600"
               >
                 <RiCalendarCloseLine class="h-4 w-4" />
-                {{ qhDates.getReadableDate(job.application_ends as Date) }}
+                {{ qhDates.getReadableDate(job?.application_ends as Date) }}
               </qh-button>
             </div>
           </div>
@@ -96,13 +96,13 @@
             label="See Applicants"
           />
           <qh-button
-            v-else-if="job.job_status === 'paused'"
+            v-else-if="job?.job_status === 'paused'"
             class="h-8 w-fit rounded-full bg-dark-500 px-8"
             label="Paused"
             disabled
           />
           <qh-button
-            v-else-if="job.is_applicant"
+            v-else-if="job?.is_applicant"
             class="disabled h-8 w-fit rounded-full bg-brand-400 px-8"
             label="Applied"
             disabled
@@ -110,7 +110,7 @@
 
           <qh-button
             v-else
-            @click="getApplyForJob(job._id)"
+            @click="getApplyForJob(job?._id)"
             class="h-8 w-fit rounded-full bg-brand px-8"
             label="Apply"
           />
